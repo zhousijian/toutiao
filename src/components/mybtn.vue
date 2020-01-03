6e4748c5684633c01eec46c91ee77d2002fc59ed
zhousijian <13553593242@163.com>
2020-01-03 17:27:43 +0800

    实现按钮的基本封装

---
+++ src/components/mybtn.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mybtn" @click="myclick">
+    <slot>插槽</slot>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        myclick(e){
+            this.$emit('mysonclick',e)
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.mybtn {
+    margin: 0 auto;
+  width: 318 * 100vw/360;
+  height: 50px;
+  background-color:rgb(74, 199, 58);
+  border-radius: 25px;
+  line-height: 50px;
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+  cursor: pointer;
+  &:hover {
+      opacity: 0.6;
+  }
+}
+</style>
