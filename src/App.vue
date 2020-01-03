c54e00b45c2b5bd424f27a5eb51d21c2ee943a47
zhousijian <13553593242@163.com>
2020-01-03 17:29:40 +0800

    一些静态资源的准备

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 
 export default {
 
 }
 </script>
 
-<style>
-
+<style lang="less">
+@import url('http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css');
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '@/style/reset.css'
+
+import { Toast } from 'vant'
+Vue.use(Toast);
+
+// 引入路由组件
+import router from '@/router/index.js'
+
 Vue.config.productionTip = false
 
 new Vue({
+  // 注入，让整个应用都可以使用路由功能
+  router,
   render: h => h(App),
 }).$mount('#app')
