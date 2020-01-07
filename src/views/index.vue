bc5f4cf26bb2c9700388af47922fa89fbcbcca98
zhousijian <13553593242@163.com>
2020-01-07 16:15:11 +0800

    完成首页的数据改造并且渲染

--- src/views/index.vue
+++ src/views/index.vue
@@ -13,39 +13,90 @@
         <van-icon name="manager" />
       </div>
     </header>
+
+    <div class="hidden"></div>
+
     <!--    导航栏    -->
     <nav>
-      <van-tabs v-model="active" sticky swipeable>
-        <van-tab v-for="(v,i) in arr" :key="i" :title="v.name">
+      <van-tabs v-model="active" sticky swipeable :offset-top=60>
+        <van-tab v-for="(v1,i1) in mydata" :key="i1" :title="v1.name">
+          <articles v-for="(v2,i2) in v1.postList" :key="i2" :DATA="v2" ></articles>
+          <!-- <articles :DATA="mydata[active].postList"></articles> -->
         </van-tab>
       </van-tabs>
     </nav>
+
     <!--    主体内容    -->
-    <main></main>
+    <main>
+      <!-- <articles :DATA='mydata[active].postList'></articles> -->
+    </main>
   </div>
 </template>
 
 <script>
-// import { articleList } from '@/apis/article'
-import { categoryList } from '@/apis/category'
+import { articleList } from "@/apis/article";
+import { categoryList } from "@/apis/category";
+import articles from "@/components/articles";
 export default {
-  data () {
+  data() {
     return {
-      active : localStorage.getItem('token')?'0':'1',
-      arr : ''
-    }
+      // id: "",
+      active: localStorage.getItem("token") ? 1 : 0,
+      mydata: ""
+    };
   },
-  async mounted () {
-    // let res = await articleList()
-    // console.log(res);
-    
+  components: {
+    articles
+  },
+  async mounted() {
+    // this.id = localStorage.getItem("id");
+    // console.log(this.id);
+
     // 获取栏目列表
-    let res = await categoryList()
+    let res = await categoryList();
     // console.log(res);
-    this.arr = res.data.data
-    console.log(this.arr);
-    
-    
+    this.mydata = res.data.data;
+    // console.log(this.mydata);
+
+    // 数据改造
+    this.mydata = this.mydata.map(value => {
+      return {
+        ...value, // 展开对象，需要拿到里面的成员
+        postList: [], // 这个栏目的新闻列表数据
+        pageSize: 10, // 这个栏目每页所显示的记录数
+        pageIndex: 1 // 这个栏目当前的页码
+      };
+    });
+    // console.log(this.mydata);
+
+    // 获取文章列表
+    let res2 = await articleList({
+      pageSize: this.mydata[this.active].pageSize,
+      pageIndex: this.mydata[this.active].pageIndex,
+      category: this.mydata[this.active].id
+    });
+    // console.log(res2);
+    this.mydata[this.active].postList = res2.data.data;
+    // console.log(this.mydata);
+    // console.log(this.mydata[this.active].postList);
+  },
+  watch: {
+    async active(){
+      // console.log(this.active);
+    // 获取文章列表
+    if(this.mydata[this.active].postList.length == 0){
+      let res2 = await articleList({
+      pageSize: this.mydata[this.active].pageSize,
+      pageIndex: this.mydata[this.active].pageIndex,
+      category: this.mydata[this.active].id
+    });
+    console.log(res2);
+    this.mydata[this.active].postList = res2.data.data;
+    // console.log(this.mydata);
+    // console.log(this.mydata[this.active].postList);
+    }
+      
+    }
   }
 };
 </script>
@@ -57,6 +108,8 @@ export default {
 header {
   display: flex;
   position: fixed;
+  z-index: 1000;
+  // margin-bottom: 50px;
   justify-content: space-between;
   align-items: center;
   width: 100%;
@@ -93,13 +146,17 @@ header {
 }
 nav {
   // margin-top: 50px;
-  position: fixed;
+  // position: fixed;
   // margin-top: 100px;
+  top: 100px;
   width: 100%;
-  margin-top: 60px;
-  background-color: #ccc;
+  // margin-top: 60px;
+  // background-color: rgb(19, 17, 17);
   /deep/.van-ellipsis {
-    background-color: rgb(230, 230, 230);
+    background-color: rgb(189, 189, 189);
   }
+}
+.hidden {
+  height: 60px;
 }
 </style>
